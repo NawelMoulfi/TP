656446c61c5056ba35dff087b351f1fff385e055
NawelMoulfi <in_moulfi@esi.dz>
2022-01-21 15:47:48 +0100

    Creation d'un utilisateur

--- src/main/java/com/telly/controllers/UserController.java
+++ src/main/java/com/telly/controllers/UserController.java
@@ -4,7 +4,9 @@ package com.telly.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
+import com.telly.dao.User;
+import com.telly.dao.FormValidationGroup ;
+import com.telly.service.UserService;
 
 
 @Controller
@@ -20,7 +22,30 @@ public class UserController {
 	public String showLogout() {
 		return "loggedout";
 	}
-	
+
+	@RequestMapping("/createaccount")
+	public String createAccount(Model model, Principal principal) {
+
+		model.addAttribute("user", new User());
+
+		return "createaccount";
+	}
+
+	@RequestMapping(value = "/createuser", method = RequestMethod.POST)
+	public String createUser(@Validated(FormValidationGroup.class) User user, BindingResult result) {
+
+		if(result.hasErrors()) {
+			return "createaccount";
+		}
+
+		user.setAuthority("ROLE_USER");
+		user.setEnabled(true);
+
+		userService.create(user);
+
+		return "home";
+
+	}
 
 }
 
